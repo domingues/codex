c6a31c1b5c36f1e16448f0d173a789b9786e70cf
Fábio Domingues <fdomingues@dcc.fc.up.pt>
2018-07-13 15:08:57 +0100

    fix type

--- src/Codex/Tester/Sql.hs
+++ src/Codex/Tester/Sql.hs
@@ -45,7 +45,7 @@ getSelectDbName :: Tester (Maybe String)
 getSelectDbName = do
   initFilePath <- metadata "db-init-file"
   case initFilePath of
-    Nothing -> Nothing
+    Nothing -> return Nothing
     Just v -> do
       testPath' <- testPath
       let initFilePath' = takeDirectory testPath' </> v
